4c2d570da4d90daaa74116d32d645aa473a679ca
Eduard <eduard.martini@onthegosystems.com>
2023-08-10 18:02:38 +0300

    [WIP] First commit

--- train.py
+++ train.py
@@ -7,7 +7,7 @@ import bitsandbytes as bnb
 from datasets import load_dataset
 from functools import partial
 import os
-from peft import LoraConfig, get_peft_model, prepare_model_for_kbit_training, AutoPeftModelForCausalLM
+# from peft import LoraConfig, get_peft_model, prepare_model_for_kbit_training, AutoPeftModelForCausalLM
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer, set_seed, Trainer, TrainingArguments, BitsAndBytesConfig, \
     DataCollatorForLanguageModeling
