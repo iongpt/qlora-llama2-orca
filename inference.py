b2016a3c59b661d00a83f0ef0c21ddb69a41a187
iongpt <135581102+iongpt@users.noreply.github.com>
2023-11-08 05:32:46 +0200

    Update inference.py
    Refactoring:
    
    Device detection for GPU has been made dynamic with torch.cuda.is_available().
    
    Configuration dictionaries have been added (MODEL_CONFIG and GENERATION_CONFIG) to keep the code DRY (Don't Repeat Yourself).
    
    Function names have been made more descriptive (generate_text and translate_using_pipeline) to clearly indicate their purpose.
    
    Added type hints for better clarity.
    
    The input_ids are now correctly moved to the model's device, which could be either CPU or GPU depending on the availability and the device setting.
    
    The device parameter for load_model_and_tokenizer has been set to a default of "auto", allowing for automatic detection of the appropriate device.
    
    The logging verbosity is set to critical inside the translate_using_pipeline function to avoid repeated code and to ensure it only affects this operation.
    
    Commented out and redundant lines have been removed to tidy up the code.

--- inference.py
+++ inference.py
@@ -1,67 +1,81 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline, logging
 import torch
+from transformers import (
+    AutoModelForCausalLM, 
+    AutoTokenizer, 
+    pipeline, 
+    logging
+)
 
+# Constants for model configuration
+MODEL_CONFIG = {
+    "max_memory_mapping": {0: "22GB"},
+    "torch_dtype": torch.float16,
+    "offload_folder": "."
+}
 
-def load_model_and_tokenizer(model_name_or_path, device):
+# Constants for text generation
+GENERATION_CONFIG = {
+    "temperature": 0.7,
+    "max_new_tokens": 512,
+    "top_p": 0.95,
+    "repetition_penalty": 1.15
+}
+
+def load_model_and_tokenizer(model_name_or_path: str, device: str = "auto") -> tuple:
     print(f"Loading base model: {model_name_or_path}")
 
-    max_memory_mapping = {0: "22GB"}
     model = AutoModelForCausalLM.from_pretrained(
         model_name_or_path,
-        device_map="auto",
-        # load_in_4bit=True,
-        max_memory=max_memory_mapping,
-        torch_dtype=torch.float16,
-        offload_folder="."
+        device_map=device,
+        **MODEL_CONFIG
     )
 
     tokenizer = AutoTokenizer.from_pretrained(model_name_or_path, use_fast=True)
     return model, tokenizer
 
 
-def generate_translation(model, tokenizer, prompt_template):
+def generate_text(model: AutoModelForCausalLM, tokenizer: AutoTokenizer, prompt: str) -> str:
     print("\n\n*** Generate:")
-    input_ids = tokenizer(prompt_template, return_tensors='pt').input_ids.cuda()
-    output = model.generate(inputs=input_ids, temperature=0.7, max_new_tokens=512)
+    input_ids = tokenizer(prompt, return_tensors='pt').input_ids
+    input_ids = input_ids.to(model.device)
+    output = model.generate(input_ids, **GENERATION_CONFIG)
     return tokenizer.decode(output[0])
 
 
-def use_pipeline_for_translation(model, tokenizer, prompt_template):
-    # Prevent printing spurious transformers error when using pipeline
-    logging.set_verbosity(logging.CRITICAL)
+def translate_using_pipeline(model: AutoModelForCausalLM, tokenizer: AutoTokenizer, prompt: str) -> str:
+    logging.set_verbosity(logging.CRITICAL)  # Suppressing unnecessary logs
 
     print("*** Pipeline:")
-    pipe = pipeline(
+    text_generator = pipeline(
         "text-generation",
         model=model,
         tokenizer=tokenizer,
-        max_new_tokens=512,
-        temperature=0.7,
-        top_p=0.95,
-        repetition_penalty=1.15
+        **GENERATION_CONFIG
     )
 
-    return pipe(prompt_template)[0]['generated_text']
+    return text_generator(prompt)[0]['generated_text']
 
 
 def main():
     model_name_or_path = "iongpt_EE_Ion_en_es-v1_0_alpha-fp16"
-    device = "auto"
+    device = "cuda" if torch.cuda.is_available() else "cpu"
     model, tokenizer = load_model_and_tokenizer(model_name_or_path, device)
 
     prompt = "Translate the phrase \"I am sleeping\" from English to Spanish"
-    system_message = "As a professional translator with expertise in English and Spanish, you are tasked with translating individual strings from English to Spanish. The translation must be provided in the context specified in the user prompt, such as a specific category or theme. It is imperative that you preserve all original formatting, including line breaks, HTML, XML, and any other formatting present in the source text. Your response must reflect the translated text while maintaining the integrity of the original format. Ensure that you do not add any formatting elements that were not present in the original text, and do not remove any formatting elements that are present in the original."
-    prompt_template = f'''[INST] <<SYS>>
-{system_message}
-<</SYS>>
+    system_message = (
+        "As a professional translator with expertise in English and Spanish, you are tasked with translating individual "
+        "strings from English to Spanish. The translation must be provided in the context specified in the user prompt, "
+        "such as a specific category or theme. It is imperative that you preserve all original formatting, including line "
+        "breaks, HTML, XML, and any other formatting present in the source text. Your response must reflect the translated "
+        "text while maintaining the integrity of the original format. Ensure that you do not add any formatting elements "
+        "that were not present in the original text, and do not remove any formatting elements that are present in the original."
+    )
+    prompt_template = f'[INST] <<SYS>>\n{system_message}\n<</SYS>>\n\n{prompt} [/INST]\n'
 
-{prompt} [/INST]
-'''
-
-    translation = generate_translation(model, tokenizer, prompt_template)
+    translation = generate_text(model, tokenizer, prompt_template)
     print(translation)
 
-    pipeline_translation = use_pipeline_for_translation(model, tokenizer, prompt_template)
+    pipeline_translation = translate_using_pipeline(model, tokenizer, prompt_template)
     print(pipeline_translation)
 
 
